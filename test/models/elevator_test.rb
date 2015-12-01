86cbf5176d3df0bc89031bbfdf45d06626041f40
Javier Alvarez <javier@crowsoft.com.ar>
2015-12-01 20:02:02 -0300

    Fix duplicated name in text unit

--- test/models/elevator_test.rb
+++ test/models/elevator_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class ElevatorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should change direction" do
+    assert false
+  end
+
   test "should accept a floor number" do
     elevator = Elevator.new
     elevator.arriveAtFloor 4
@@ -16,22 +18,20 @@ class ElevatorTest < ActiveSupport::TestCase
     assert_equal elevator.requestedFloor, [0,5,3]
   end
 
-  test "should change direction" do
-    assert false
-  end
-  test "should accept a floor number" do
-    assert false
-  end
   test "should move to that floor" do
     assert false
   end
+
   test "should mantain a list of floor numbers" do
     assert false
   end
+
   test "should move through the floors" do
     assert false
   end
+
   test "should reverse direction when it reaches the top" do
     assert false
   end
+
 end
